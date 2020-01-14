0cc764f413451c684e9294c1a0eda3529347eb68
lawrencejiabin <lawrencejiabin@163.com>
2020-01-14 15:59:30 +0800

    delete emacs-20181224

--- emacs-config/emacs-20181224/.emacs.d/elpa/string-inflection-20170419.1638/string-inflection-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "string-inflection" "20170419.1638" "underscore -> UPCASE -> CamelCase -> lowerCamelCase conversion of names" 'nil :keywords '("elisp"))
--- emacs-config/emacs-20181224/.emacs.d/elpa/symon-20170224.33/symon-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "symon" "20170224.33" "tiny graphical system monitor" 'nil :url "http://hins11.yu-yake.com/")
